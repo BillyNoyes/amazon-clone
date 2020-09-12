25713c24d5fc5e2ff5e2d0b32670776e559cf341
William (Billy) Noyes <wbillypn@gmail.com>
2020-09-12 21:49:20 +0100

    Created Header component and installed MUI

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDbMweFL8KM4uI59NxBuUlOQNtXKbXP2-k",
+  authDomain: "fir-a6e48.firebaseapp.com",
+  databaseURL: "https://fir-a6e48.firebaseio.com",
+  projectId: "fir-a6e48",
+  storageBucket: "fir-a6e48.appspot.com",
+  messagingSenderId: "445559460335",
+  appId: "1:445559460335:web:08263bf026d010fba3661a",
+};
