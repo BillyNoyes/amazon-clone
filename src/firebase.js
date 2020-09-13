4c90029d8490fb979794be0dba85495cde277952
William (Billy) Noyes <wbillypn@gmail.com>
2020-09-13 18:30:18 +0100

    Configured the firebase settings

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDbMweFL8KM4uI59NxBuUlOQNtXKbXP2-k",
   authDomain: "fir-a6e48.firebaseapp.com",
@@ -7,3 +9,10 @@ const firebaseConfig = {
   messagingSenderId: "445559460335",
   appId: "1:445559460335:web:08263bf026d010fba3661a",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
